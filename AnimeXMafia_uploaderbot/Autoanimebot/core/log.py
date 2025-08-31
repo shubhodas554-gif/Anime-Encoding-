3af1938d6843fed04e73832b1b79785e671fd59c
shubhodas554-gif <shubhodas554@gmail.com>
2025-08-31 14:50:10 +0530

    Create log.py

---
+++ AnimeXMafia_uploaderbot/Autoanimebot/core/log.py
@@ -0,0 +1,11 @@
+import logging
+
+def LOGGER(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
+    logger.addHandler(handler)
+    return logger
